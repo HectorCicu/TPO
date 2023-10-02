96197ee14a6c9b2d46a18413b795a63a1a436810
Hector Cicutti <hector.cicutti@gmail.com>
2023-10-02 19:27:43 -0300

    v1.0.2

--- javascript/getApi.js
+++ javascript/getApi.js
@@ -1,36 +1,53 @@
-let carousel = document.getElementById("carousel-ofertas");
+
+
+const carousel = document.querySelector("#carousel-ofertas");
+let result;
+let resulta;
+
 async function getAllProducts() {
-  let response = await fetch(" https://api.escuelajs.co/api/v1/products");
-  let { results } = await response.json();
-  return results;
+  
+  // let response = await fetch("https://api.escuelajs.co/api/v1/products");
+  let response = await fetch("https://api.escuelajs.co/api/v1/products");
+  let results = await response.json();
   console.log(results);
+
+  //console.log( "datos " + data);
+  // results.forEach((e) => {
+  //   console.log("imagenes " + e.images[0]);
+  // });
+
+  return results;
 }
-console.log("uno");
+
+function agregarImg(n) {
+  m = "./img/Logo.png";
+  
+  imagen = `<img src="${n.images[0]}" class="d-block w-40" alt="${n.title}" />`;
+  //console.log("IMAGEN =" + imagen);
+  //document.body.appendChild(img)
+  return imagen;
+}
+
 var imagen;
 var armado = '<div class="carousel-item active">';
 var finArmado = "</div>";
-var armadoTotal;
-function agregarImg(n) {
-  console.log("dos");
-  imagen = document.createElement("img");
-  imagen.src = n;
-  imagen.alt = n;
-  return imagen;
-}
-console.log("tres");
-var results;
-function incorporoImg() {
-  console.log("cuatro");
-  let a = async function () {
-    results = await getAllProducts();
-    console.log(1);
-    console.log(results);
-    let contador = 0;
-    results.forEach((e) => {
-      ++contador;
-      armadoTotal =
-        armadoTotal + armado + agregarImg(e.category.image) + finArmado;
-    });
-    carousel.innerHTML = armadoTotal;
-  };
-}
+var armadoTotal = "";
+// function incorporoImg() {
+//   let a = async function () {};
+// }
+var app = '';
+document.addEventListener("DOMContentLoaded", async function () {
+  var resulta = await getAllProducts();
+  let contador = 0;
+  resulta.forEach((e) => {
+    ++contador;
+
+    if (contador < 8) {
+    
+      armadoTotal = armadoTotal + armado + agregarImg(e) + finArmado;
+
+    }
+  });
+  carousel.innerHTML = armadoTotal;
+  console.log("armado " +armadoTotal);
+});
