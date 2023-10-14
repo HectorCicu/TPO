16229391f2eb15857598f282a3a7c89e1944e667
Hector Cicutti <hector.cicutti@gmail.com>
2023-10-14 01:25:30 -0300

    Agregar datos API OMDB

--- javascript/apiGnews.js
+++ javascript/apiGnews.js
@@ -1,5 +1,6 @@
-apikey = 'bf3c363e5cfbeb0bf73c76c80195af39';
-url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
+apikey = "bf3c363e5cfbeb0bf73c76c80195af39";
+url =
+  "https://gnews.io/api/v4/search?q=none&apikey=" +  apikey;
 
 fetch(url)
   .then(function (response) {
@@ -7,17 +8,18 @@ fetch(url)
   })
   .then(function (data) {
     articles = data.articles;
-console.log(articles.length)
+    console.log(articles)
+    console.log(articles.length);
     for (i = 0; i < articles.length; i++) {
       // articles[i].title
-      console.log("Title: " + articles[i]['title']);
+      console.log("Title: " + articles[i]["title"]);
       // articles[i].description
-      console.log("Description: " + articles[i]['description']);
-      // You can replace {property} below with any of the article properties returned by the API.
+      console.log("Description: " + articles[i]["description"]);
+      
+      console.log('images: ' + articles[i]['image']);// You can replace {property} below with any of the article properties returned by the API.
       // articles[i].{property}
       // console.log(articles[i]['{property}']);
 
       // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-    
     }
   });
