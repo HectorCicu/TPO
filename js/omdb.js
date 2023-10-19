7185d490c04fbbc9034eb36e6ee816d9865449fe
Hector Cicutti <hector.cicutti@gmail.com>
2023-10-18 23:39:11 -0300

    Modificar logica api

--- js/omdb.js
+++ js/omdb.js
@@ -1,10 +1,10 @@
 const carousel = document.querySelector("#carousel-examples");
 
 let omdbKey = "2677da8c";
-let url = "http://www.omdbapi.com/?i=tt3896198&apikey=";
-let url2 = "http://www.omdbapi.com/?apikey=";
-let urlEjemplo =
-  'http://www.omdbapi.com/?apikey=2677da8c&&s="the"&plot=full&page=20&type="series"';
+// let url = "http://www.omdbapi.com/?i=tt3896198&apikey=";
+// let url2 = "http://www.omdbapi.com/?apikey=";
+// let urlEjemplo =
+//   'http://www.omdbapi.com/?apikey=2677da8c&&s="the"&plot=full&page=20&type="series"';
 
 const divOpen = "";
 var armado = `<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner">`;
@@ -44,23 +44,46 @@ document.addEventListener("DOMContentLoaded", async () => {
       `http://www.omdbapi.com/?&apikey=${omdbKey}&s="${busqueda}"&plot=full&page=${pagina}&type="movie"` // noticias de Argentina
     );
     const result = await response.json();
-    // console.log(result.Search);
+    console.log(result);
 
-    for (let i = 0; i < result.Search.length; ++i) {
-  //  console.log(result.Search[i].Title);
-      if (i == 0) {
-        html +=
-          `<div class="carousel-item active">` + crearTarjeta(result.Search[i]);
-      } else {
-        html += `<div class="carousel-item">` + crearTarjeta(result.Search[i]);
-      }
+    //   for (let i = 0; i < result.Search.length; ++i) {
+    // //  console.log(result.Search[i].Title);
+    //     if (i == 0) {
+    //       html +=
+    //         `<div class="carousel-item active">` + crearTarjeta(result.Search[i]);
+    //     } else {
+    //       html += `<div class="carousel-item">` + crearTarjeta(result.Search[i]);
+    //     }
+    //   }
+    if (urlInfo.includes("index.html")) {
+      armar1(result);
+    } else if (urlInfo.includes("infoVideos.html")) {
+      listar(result);
     }
   } catch (error) {
     console.error(error);
   }
+
+  // var nuevaCard = document.createElement("div");
+  // carousel.innerHTML = armado + html + finArmado;
+});
+
+///////////////////////////
+
+const armar1 = (result) => {
+  for (let i = 0; i < result.Search.length; ++i) {
+    //  console.log(result.Search[i].Title);
+    if (i == 0) {
+      html +=
+        `<div class="carousel-item active">` + crearTarjeta(result.Search[i]);
+    } else {
+      html += `<div class="carousel-item">` + crearTarjeta(result.Search[i]);
+    }
+  }
   var nuevaCard = document.createElement("div");
   carousel.innerHTML = armado + html + finArmado;
-});
+};
+/////////////////////////////
 
 function crearTarjeta(e) {
   //armo la tarjeta que contendrá la película en el carousel
@@ -69,3 +92,5 @@ function crearTarjeta(e) {
      <img src="${e.Poster}" class="d-block w-45" alt="${e.Title}">
         </div>`;
 }
+
+const listar = (result) => {};//armar la lista de peliculas para mostrar
