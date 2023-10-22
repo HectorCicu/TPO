78c188a06ceb00351055836b23904c9b6b901b33
Hector Cicutti <hector.cicutti@gmail.com>
2023-10-22 11:04:33 -0300

    Depura codigo y organiza js

--- js/omdb.js
+++ js/omdb.js
@@ -2,7 +2,7 @@ const carousel = document.querySelector("#carousel-examples");
 
 let omdbKey = "2677da8c";
 
-const divOpen = "";
+
 var armado = `<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner">`;
 var finArmado = "</div>";
 var armadoTotal = "";
@@ -16,39 +16,7 @@ var tipoVideo = "series";
 ////  comienzo la carga de las películas //////
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    // parametro = Math.round(Math.random() * 1000, 0) % 5;
-    // console.log(parametro);
-    // switch (
-    //   parametro //hago búsquedas 'randoms'
-    // ) {
-    //   case 1:
-    //     busqueda = "the";
-    //     break;
-    //   case 2:
-    //     busqueda = "and";
-    //     break;
-    //   case 3:
-    //     busqueda = "war";
-    //     break;
-    //   case 4:
-    //     busqueda = "why";
-    //     break;
-    //   default:
-    //     busqueda = "for";
-    // }
-    // // console.log(`http://www.omdbapi.com/?&apikey=${omdbKey}&s="${busqueda}"&plot=full&page=${pagina}&type="movie"`)
-
-    // const response = await fetch(
-    //   `http://www.omdbapi.com/?&apikey=${omdbKey}&s="${busqueda}"&page=${pagina}`
-    // );
-    // const result = await response.json();
-
-    // if (urlInfo.includes("index.html")) {
-    //   armar1(result);
-    // } else if (urlInfo.includes("infoVideos.html")) {
-    //   console.log("result " + result);
-    //   listar(result);
-    // }
+  
     generoFetch(tipoVideo);
   } catch (error) {
     console.error(error);
@@ -119,46 +87,7 @@ const armar1 = (result) => {
 
 
 
-const listar = (result) => {
-  limpiarTabla();
-  let tabla = document.getElementById("tbody-table");
 
-  for (let i = 0; i < result.Search.length; ++i) {
-    // console.log(result.Search[i].Title);
-    // console.log(result.Search[i].Year);
-    // console.log(result.Search[i].imdbID);
-    // console.log(result.Search[i].Type);
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    td1.textContent = result.Search[i].Title;
-    tr.appendChild(td1);
-
-    let td2 = document.createElement("td");
-    td2.textContent = result.Search[i].Year;
-    tr.appendChild(td2);
-
-    let td3 = document.createElement("td");
-    td3.textContent = result.Search[i].imdbID;
-    tr.appendChild(td3);
-
-    let td4 = document.createElement("td");
-    td4.textContent = result.Search[i].Type;
-    tr.appendChild(td4);
-    //agrego un botón para seleccionar cualquiera de los
-    let td5 = document.createElement("td");
-    let boton = document.createElement("button");
-    boton.textContent = `Detalle`;
-    boton.id = `botonBuscarVideo`;
-    boton.value = `${result.Search[i].imdbID}`;
-    boton.onclick = () => {
-      buttonClicked(boton.value);
-    };
-    td5.appendChild(boton);
-    tr.appendChild(td5);
-
-    tabla.appendChild(tr);
-  }
-}; //armar la lista de peliculas para mostrar
 
 /// limpio la tabla para cuando se realiza búsqueda 
 
@@ -172,30 +101,6 @@ const limpiarTabla = () => {
 };
 
 
-function buttonClicked(button) {
-  let aside = document.getElementById("aside2");
-  let h2InfoVideos = document.getElementById("titpeli");
-  let imgVideo = document.getElementById("imgPeli");
-  let genero = document.getElementById("genre");
-  let elenco = document.getElementById("elenco");
-  fetch(
-    `http://www.omdbapi.com/?i=${button}&apikey=${omdbKey}&y=&plot=short&r=json`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      if (!data.Error) {
-        h2InfoVideos.innerHTML = data.Title;
-        imgVideo.innerHTML = ` <img src=${data.Poster} alt=${data.Title} id="poster">`;
-        genero.innerHTML = `<strong>Género:</strong>  ${data.Genre}`;
-        elenco.innerHTML = `<strong>Elenco:</strong>  ${data.Actors}`;
-      } else {
-        alert("No se ha podido encontrar el titulo");
-      }
-    });
-
-  console.log("Botón presionado: " + button);
-  // Realiza cualquier otra acción necesaria
-}
 
 
 /** Genero los eventos para ver si presiono ver películas o series */
--- js/videos.js
+++ js/videos.js
@@ -1,11 +1,83 @@
 /** este js es para buscar películas o series */
+const listar = (result) => {
+  limpiarTabla();
+  let tabla = document.getElementById("tbody-table");
 
+  for (let i = 0; i < result.Search.length; ++i) {
+    // console.log(result.Search[i].Title);
+    // console.log(result.Search[i].Year);
+    // console.log(result.Search[i].imdbID);
+    // console.log(result.Search[i].Type);
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.textContent = result.Search[i].Title;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement("td");
+    td2.textContent = result.Search[i].Year;
+    tr.appendChild(td2);
+
+    let td3 = document.createElement("td");
+    td3.textContent = result.Search[i].imdbID;
+    tr.appendChild(td3);
+
+    let td4 = document.createElement("td");
+    td4.textContent = result.Search[i].Type;
+    tr.appendChild(td4);
+    //agrego un botón para seleccionar cualquiera de los
+    let td5 = document.createElement("td");
+    let boton = document.createElement("button");
+    boton.textContent = `Detalle`;
+    boton.id = `botonBuscarVideo`;
+    boton.value = `${result.Search[i].imdbID}`;
+    boton.onclick = () => {
+      botonClickeado(boton.value);
+    };
+    td5.appendChild(boton);
+    tr.appendChild(td5);
+
+    tabla.appendChild(tr);
+  }
+}; //armar la lista de peliculas para mostrar
+
+function botonClickeado(button) {
+  let aside = document.getElementById("aside2");
+  let h2InfoVideos = document.getElementById("titpeli");
+  let imgVideo = document.getElementById("imgPeli");
+  let genero = document.getElementById("genre");
+  let elenco = document.getElementById("elenco");
+  let resumen = document.getElementById("resumen")
+  fetch(
+    `http://www.omdbapi.com/?i=${button}&apikey=${omdbKey}&y=&plot=short&r=json`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.Error) {
+        h2InfoVideos.innerHTML = data.Title;
+        imgVideo.innerHTML = ` <img src=${data.Poster} alt=${data.Title} id="poster">`;
+        genero.innerHTML = `<strong>Género:</strong>  ${data.Genre}`;
+        elenco.innerHTML = `<strong>Elenco:</strong>  ${data.Actors}`;
+     resumen.innerHTML = `<strong>Resumen:</strong>  ${data.Plot}`;
+      } else {
+        alert("No se ha podido encontrar el titulo");
+      }
+    });
+
+  console.log("Botón presionado: " + button);
+  // Realiza cualquier otra acción necesaria
+}
 document.addEventListener("DOMContentLoaded", async () => {
-  
-    let elijoPeli = document.getElementById("movies");
-    let elijoSerie = document.getElementById("series");  elijoSerie.addEventListener("click", (e) => {
+  let buscarTitulo = document.getElementById("busqueda");
+
+  let elijoPeli = document.getElementById("movies");
+  let elijoSerie = document.getElementById("series");
+
+  elijoSerie.addEventListener("click", (e) => {
     e.preventDefault();
+    buscarTitulo.value = "";
     try {
+      tipoVideo = "series";
+
       console.log("vamos por series");
       generoFetch("series");
     } catch (error) {
@@ -15,28 +87,33 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   elijoPeli.addEventListener("click", (e) => {
     e.preventDefault();
+    buscarTitulo.value = "";
     try {
+      tipoVideo = "movie";
       generoFetch("movie");
     } catch (error) {
       console.log(error);
     }
   });
-  let buscarTitulo = document.getElementById("busqueda");
 
   buscarTitulo.addEventListener("input", async (e) => {
     const ingresoLetra = e.target.value;
+
     try {
       if (ingresoLetra.length >= 3) {
         const res = await fetch(
           `http://www.omdbapi.com/?&apikey=${omdbKey}&s="${ingresoLetra}&type=${tipoVideo}`
         );
         const resultado = await res.json();
-        console.log(ingresoLetra);
-        console.log(resultado);
+        // console.log(ingresoLetra);
+        // console.log(resultado);
         listar(resultado);
       }
     } catch (error) {
       console.log(error);
     }
   });
+
+  
+  
 });
